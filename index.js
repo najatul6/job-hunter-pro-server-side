b60b48c34366c74c90b3df9059b7b22daae164be
najatul6 <mdnajatulislam.develop@gmail.com>
2023-11-08 00:07:52 +0600

    Create usercollection and jobcollection

--- index.js
+++ index.js
@@ -3,20 +3,16 @@ const app = express();
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
-const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const cookieParser = require('cookie-parser');
 
 // MiddleWare 
-app.use({
-    origin:'http://localhost:5173/',
-    credentials:true
-});
 app.use(express.json());
-app.use(cookieParser())
+app.use(cors());
+
+
 
 // MongoDB URI 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.trhzw6v.mongodb.net/jobhunterproDB?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.trhzw6v.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -32,98 +28,41 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
-        const jobCollection = client.db('jobhunterproDB').collection('alljobs')
+        const jobCollection = client.db('jobhunterproDB').collection('allJobs')
         const appliedCollection = client.db('jobhunterproDB').collection('allappliedjobs')
+        const userCollection = client.db('jobhunterproDB').collection('allUsers')
 
-        // verify Token 
-        const verifyToken = (req, res, next) => {
-            const token = req.cookies.token
-            if (!token) {
-                return res.status(401).send({ message: 'UnAuthorized' })
+        app.get('/allJobs', async(req,res)=>{
+            const keyword = req.query.keyword;
+            let query = {}
+            if(keyword){
+                 query = { jobTitle: { $regex: new RegExp(keyword, 'i') }}
             }
-
-            jwt.verify(token, process.env.ACCESS_TOKEN_SECTRET, function (err, decoded) {
-                if (err) {
-                    return res.status(401).send({ message: 'UnAuthorized' })
-                }
-                req.user = decoded
-                next()
-            });
-        }
-
-        app.get('/api/v1/alljobs', async (req, res) => {
-            const result = await jobCollection.find().toArray()
+            // console.log(keyword)
+            const cursor = jobCollection.find(query);
+            const result = await cursor.toArray();
             res.send(result)
         })
 
-        // Applied Job collection create 
-        app.get('/api/v1/user/allappliedjobs', verifyToken, async (req, res) => {
-            const clientEmail = req.body.email;
-            const tokenEmail = req.user.email;
-            if (clientEmail !== tokenEmail) {
-                return res.status(403).send({message:'Forbidden'})
-            }
-            let query ={}
-            if(clientEmail){
-                query.email=clientEmail
-            }
-            const result = await appliedCollection.find(query).toArray()
-            res.send(result)
-        })
-
-        // ALL Job collection insert 
-        app.post('/api/v1/alljobs', async (req, res) => {
-            const newJob = req.body;
-            const result = await jobCollection.insertOne(newJob)
-            res.send(result)
-        })
-
-        // Applied Job collection create 
-        app.post('/api/v1/user/allappliedjobs', async (req, res) => {
-            const newapplication = req.body;
-            const result = await appliedCollection.insertOne(newapplication)
-            res.send(result)
-        })
-
-        // Authentication Jwt Token Create 
-        app.post('/api/v1/auth/access-token', async (req, res) => {
-            const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECTRET)
-            res.cookie('token', token, {
-                httpOnly: true,
-                secure: true,
-                sameSite: 'none'
-            }).send({ success: true })
-        })
-
-        // Delete from all job collection 
-        app.delete('/api/v1/user/cancel-job/:id', async (req, res) => {
+        app.get('/allJobs/:id', async(req,res)=>{
             const id = req.params.id;
-            const result = await jobCollection.deleteOne({ _id: new ObjectId(id) })
+            const query = {_id: new ObjectId(id)};
+            const result = await jobCollection.findOne(query)
             res.send(result)
         })
 
+        // Applyed 
+        app.post('/allappliedjobs', async(req, res)=>{
+            const appliedjob = req.body;
+            console.log(appliedjob)
+        })
 
-        // Update Post 
-        app.put('/api/v1/user/update-job/:id', async (req, res) => {
-            const id = req.params.id;
-            const filter = { _id: new ObjectId(id) }
-            const options = { upsert: true };
-            const updateJob = req.body;
-            const JobPreview = {
-                $set: {
-                    productImg: updateJob.productImg,
-                    productName: updateJob.productName,
-                    productBrand: updateJob.productBrand,
-                    productType: updateJob.productType,
-                    productDescription: updateJob.productDescription,
-                    productPrice: updateJob.productPrice,
-                    productRating: updateJob.productRating,
-
-                }
-            }
-            const result = await jobCollection.updateOne(filter, JobPreview, options);
-            res.send(result);
+        //All User
+        app.post('/allusers', async(req, res)=>{
+            const newUser = req.body;
+            console.log(newUser);
+            const result = await userCollection.insertOne(newUser)
+            res.send(result)
         })
 
         // Send a ping to confirm a successful connection
